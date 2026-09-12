b0b1bc66cf0213aebde33752f6957b9022f067bc
bunsokha-panha <panhabun097@gmail.com>
2026-09-12 15:14:06 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,81 @@
-import React from 'react'
+
 import { NavLink } from 'react-router-dom'
 import { RiReactjsFill } from "react-icons/ri";
 import shopLogo from '../assets/shop_logo.png'
+import React, { useState } from 'react'
+import { RxDragHandleHorizontal } from "react-icons/rx";
 
 function Navbar() {
+
+    const [isMobile, setIsMobile] = useState(false)
+
+    function toggleHandle(){
+        setIsMobile(!isMobile)
+    }
+
   return (
-      <div className='bg-gray-200'>
-        <ul className='flex gap-5 px-10 py-3 items-center'>
-          <img src={shopLogo} alt="Shop Logo" className='h-15 w-15' />
-          <li>
-            <NavLink 
-              className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
-              to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink 
-              className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
-              to="/product">Product</NavLink>
-          </li>
-          <li>
-            <NavLink 
-              className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
-              to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink 
-              className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
-              to="/create">Create</NavLink>
-          </li>
+    <>
+        <div className='bg-gray-200 flex justify-between px-8 md:px-10 lg:px-20 items-center'>
+            <ul className='gap-3 lg:gap-5 items-center hidden md:flex'>
+              <img src={shopLogo} alt="Shop Logo" className='h-15 w-15 ' />
+              <li>
+                <NavLink 
+                  className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/product">Product</NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  className={({isActive})=> isActive ? "text-white font-bold p-3 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/create">Create</NavLink>
+              </li>
+            </ul>
+            <div className='hidden md:flex items-center gap-6'>
+                <input type="text" placeholder='Search' className='p-1 border border-gray-500 rounded-lg focus:outline-hidden'/>
+                <button className='bg-blue-500 text-white px-3 rounded-lg py-1'>Search</button>
+            </div>
+            <div className='md:hidden flex w-full justify-between items-center gap-3'>
+              <img src={shopLogo} alt="Shop Logo" className='h-15 w-15' />
+              <RxDragHandleHorizontal onClick={toggleHandle} className='text-3xl block' />  
+            </div>
+        </div>
+
+        <ul className={`flex md:hidden flex-col items-start gap-6 px-8  overflow-hidden transition-all duration-300 ease-in-out bg-gray-100  ${isMobile ? "max-h-96" : "max-h-0"}`}>
+            <li className='w-full mt-3'>
+                <NavLink 
+                  className={({isActive})=> isActive ? "block w-full text-white font-bold px-1 py-2 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/">Home</NavLink>
+              </li>
+              <li className='w-full'>
+                <NavLink 
+                  className={({isActive})=> isActive ? "block w-full text-white font-bold px-1 py-2 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/product">Product</NavLink>
+              </li>
+              <li className='w-full'>
+                <NavLink 
+                  className={({isActive})=> isActive ? "block w-full text-white font-bold px-1 py-2 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/about">About</NavLink>
+              </li>
+              <li className='w-full'>
+                <NavLink 
+                  className={({isActive})=> isActive ? "block w-full text-white font-bold px-1 py-2 bg-blue-500 rounded-lg transition-all duration-300 ease-in-out" : "text-black"} 
+                  to="/create">Create</NavLink>
+              </li>
+              <li className='flex gap-5 mb-5'>
+                  <input type="text" placeholder='Search' className='w-auto p-1 border border-gray-500 rounded-lg focus:outline-hidden'/>
+                  <button className='bg-blue-500 text-white px-3 rounded-lg py-1'>Search</button>
+              </li>
         </ul>
-      </div>
+    </>
   )
 }
 
